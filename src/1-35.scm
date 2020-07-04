d97cae4568b5b37d3deb2170930b2db5738619b8
Florian Kretlow <floriankretlow@gmx.de>
2020-07-04 12:22:25 +0200

    oh well, stick to ascii chars then

--- src/1-35.scm
+++ src/1-35.scm
@@ -1,8 +1,8 @@
 ; Exercise 1.35
 ; =============
 
-; Show that the golden ratio φ (Section 1.2.2) is a fixed point of the
-; transformation x -> 1 + 1/x, and use this fact to compute φ by means of the
+; Show that the golden ratio phi (Section 1.2.2) is a fixed point of the
+; transformation x -> 1 + 1/x, and use this fact to compute phi by means of the
 ; ``fixed-point`` procedure.
 
 
@@ -23,6 +23,6 @@
 ; Let a, b, and c be any three consecutive Fibonacci numbers.
 ; Then the ratio between a and b is r_n = b / a.
 ; The next ratio is r_n+1 = c / b = (a+b) / b = 1 + a/b = 1 + 1 / r_n.
-; Thus φ is a fixed point of the transformation x -> 1 + 1/x.
+; Thus phi is a fixed point of the transformation x -> 1 + 1/x.
 
 (define golden-ratio (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
