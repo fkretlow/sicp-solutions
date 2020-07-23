6e937f8e79159e9c74c52197f7b9df1396d5d0d6
Florian Kretlow <floriankretlow@gmx.de>
2020-07-23 10:15:12 +0200

    2.12

--- 2/interval-arithmetic.scm
+++ 2/interval-arithmetic.scm
@@ -181,3 +181,34 @@
                                     (* (upper-bound x) (lower-bound y)))
                                (max (* (lower-bound x) (lower-bound y))
                                     (* (upper-bound x) (upper-bound y)))))))))
+
+
+; After debugging her program, Alyssa shows it to a potential user, who
+; complains that her program solves the wrong problem. He wants a program that
+; can deal with numbers represented as a center value and an additive
+; tolerance. [...]
+
+(define (make-center-width c w) (make-interval (- c w) (+ c w)))
+(define (center i) (/ (+ (lower-bound i) (upper-bound i)) 2.0))
+(define (width i)  (/ (- (upper-bound i) (lower-bound i)) 2.0))
+
+; Unfortunately, most of Alyssa's users are engineers. Real engineering
+; situations usually involve measurements with only a small uncertainty,
+; measured as the ratio of the width of the interval to the midpoint of the
+; interval. Engineers usually specify percentage tolerances on the parameters
+; of devices. [...]
+
+
+; Exercise 2.12
+; -------------
+;
+; Define a constructor ``make-center-percent`` that takes a center and a
+; percentage tolerance and produces the desired interval. You must also define
+; a selector ``percent`` that produces the percentage tolerance for a given
+; interval. The ``center`` selector is the same as the one shown above.
+
+(define (make-center-percent c p)
+  (let ((w (* c (/ p 100.0))))
+    (make-interval (- c w) (+ c w))))
+
+(define (percent i) (* (/ (width i) (center i)) 100))
