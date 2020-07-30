bf5fdf2b56aaefffa505b23d2353e8ac88e8ce5f
Florian Kretlow <floriankretlow@gmx.de>
2020-07-30 19:51:07 +0200

    2.23

---
+++ 2/2-23.scm
@@ -0,0 +1,29 @@
+; Exercise 2.23
+; =============
+;
+; The procedure ``for-each`` is similar to ``map``. It takes as arguments a
+; procedure and a list of elements. However, rather than forming a list of the
+; results, ``for-each`` just applies the procedure to each of the elements in
+; turn, from left to right. The values returned by applying the procedure to
+; the elements are not used at all -- ``for-each`` is used with procedures that
+; perform an action, such as printing. For example, ::
+;
+;    (for-each (lambda (x)
+;                (newline)
+;                (display x))
+;              (list 57 321 88))
+;    57
+;    321
+;    88
+;
+; The value returned by the call to ``for-each`` (not illustrated above) can be
+; something, arbitrary, such as true. Give an implementation of ``for-each``.
+
+(define (for-each f xs)
+  (if (null? xs)
+      1
+      (begin (f (car xs))
+             (for-each f (cdr xs)))))
+
+; The ``begin`` expression is needed to execute both expressions in the else
+; branch. (In contrast, ``cond`` has an implicit begin in its branches.)
