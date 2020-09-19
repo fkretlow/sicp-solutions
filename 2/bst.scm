1e7cf4a172ddec3ebd3de98f778cbe4b4c1b0af9
Florian Kretlow <floriankretlow@gmx.de>
2020-09-19 14:27:07 +0200

    use built-in < predicates for primitive key types

--- 2/bst.scm
+++ 2/bst.scm
@@ -1,17 +1,18 @@
 ; BINARY SEARCH TREE
 ; - (make-bst)               -> returns a new empty tree
 ; - (bst-set tree key value) -> sets the value associated with key and returns the modified tree
-; - (bst-get tree key)       -> returns the value associated with key or bst-null-value if key doesn't exist
+; - (bst-get tree key nil)   -> returns the value associated with key or nil if key doesn't exist
 ; - (bst-delete tree key)    -> removes the key-value pair associated with key and returns the modified tree
 ; - (bst-has? tree key)      -> returns #t if key exists in the tree
 ; - (bst-flatten tree)       -> returns a sorted list of all key-value pairs in the tree
 
-; Change these if needed:
-; 1. Define an ordering for the keys.
-(define (bst-less? a b) (< a b))
-(define (bst-equal? a b) (equal? a b))
-; 2. Define a null-value to return if a requested key is not found.
-(define bst-null-value 0)
+(define (_bst-less? a b)
+  (cond ((number? a) (< a b))
+        ((char? a) (char<? a b))
+        ((string? a) (string<? a b))
+        ((symbol? a) (symbol<? a b))))
+
+(define (_bst-equal? a b) (equal? a b))
 
 (define (make-bst) '())
 (define (_bst-make-node key value left right) (list (cons key value) left right))
@@ -37,9 +38,9 @@
 (define (bst-set node key value)
   (cond ((null? node)
          (_bst-make-node key value '() '()))
-        ((bst-equal? key (_bst-key node))
+        ((_bst-equal? key (_bst-key node))
          (_bst-make-node key value (_bst-left-child node) (_bst-right-child node)))
-        ((bst-less? key (_bst-key node))
+        ((_bst-less? key (_bst-key node))
          (_bst-make-node (_bst-key node)
                         (_bst-value node)
                         (bst-set (_bst-left-child node) key value)
@@ -50,21 +51,21 @@
                          (_bst-left-child node)
                          (bst-set (_bst-right-child node) key value)))))
 
-(define (bst-get node key)
-  (cond ((null? node) bst-null-value)
-        ((bst-equal? key (_bst-key node)) (_bst-value node))
-        ((bst-less? key (_bst-key node)) (bst-get (_bst-left-child node) key))
+(define (bst-get node key nil)
+  (cond ((null? node) nil)
+        ((_bst-equal? key (_bst-key node)) (_bst-value node))
+        ((_bst-less? key (_bst-key node)) (bst-get (_bst-left-child node) key))
         (else (bst-get (_bst-right-child node) key))))
 
 (define (bst-has? node key)
   (cond ((null? node) #f)
-        ((bst-equal? key (_bst-key node)) #t)
-        ((bst-less? key (_bst-key node)) (bst-get (_bst-left-child node) key))
+        ((_bst-equal? key (_bst-key node)) #t)
+        ((_bst-less? key (_bst-key node)) (bst-get (_bst-left-child node) key))
         (else (bst-get (_bst-right-child node) key))))
 
 (define (bst-delete node key)
   (cond ((null? node) node)
-        ((bst-equal? key (_bst-key node))
+        ((_bst-equal? key (_bst-key node))
          (cond ((and (_bst-has-left-child? node)
                      (not (_bst-has-right-child? node)))
                 (_bst-left-child node))
@@ -81,7 +82,7 @@
                                   (_bst-left-child node)
                                   (bst-delete (_bst-right-child node)
                                               (_bst-key swap-node)))))))
-        ((bst-less? key (_bst-key node))
+        ((_bst-less? key (_bst-key node))
          (_bst-make-node (_bst-key node)
                         (_bst-value node)
                         (bst-delete (_bst-left-child node) key)
